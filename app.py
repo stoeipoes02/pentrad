23725720b050e567b3ae2d0182d128a15a574746
kick <kickbuur@gmail.com>
2022-12-19 21:56:20 +0100

    about page

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/about')
+def about():
+
+    return render_template('about.html')
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
