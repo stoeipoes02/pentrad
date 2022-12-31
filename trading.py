4c60a69eec6ae48be7541a60f9f3f45f0dd8f6fa
kick <kickbuur@gmail.com>
2022-12-31 19:14:06 +0100

    new secret api key added

--- trading.py
+++ trading.py
@@ -5,6 +5,6 @@ import sys
 sys.path.append("../")
 import creds
 
-client = bybit.bybit(test=False, api_key="MYb1xx5hLrv6UqET1x", api_secret=creds.api_secret)
-print("does this work?", creds.api_secret)
+client = bybit.bybit(test=False, api_key="vzgFHwAsjwMBhlRgtb", api_secret=creds.api_secret)
+
 print("loggedin")
