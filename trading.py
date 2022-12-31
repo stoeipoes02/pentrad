9fcca5f1c6c05e673b360fe67ae3d2155b2fd346
kick <kickbuur@gmail.com>
2022-12-31 18:35:29 +0100

    wachtwoord naar lokaal

--- trading.py
+++ trading.py
@@ -1,5 +1,5 @@
 import time
 import bybit
 
-client = bybit.bybit(test=False, api_key="./config.api_key", api_secret="NjUvgNWnQVOVF0rAiilcBGGodj1LaipeiErJ")
+client = bybit.bybit(test=False, api_key="./config.api_key", api_secret="./config.api_secret")
 print("loggedin")
