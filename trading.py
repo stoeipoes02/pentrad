188cff2f434fafca27223b2f944c7a1f7fbde591
kick <kickbuur@gmail.com>
2023-01-01 17:35:26 +0100

    api first working form

--- trading.py
+++ trading.py
@@ -1,10 +1,14 @@
 import time
-import bybit
 import sys
+import bybit
+from pybit import inverse_perpetual
 
 sys.path.append("../")
 import creds
 
-client = bybit.bybit(test=False, api_key="vzgFHwAsjwMBhlRgtb", api_secret=creds.api_secret)
-
-print("loggedin")
+session_auth = inverse_perpetual.HTTP(
+    endpoint="https://api.bybit.com",
+    api_key="vzgFHwAsjwMBhlRgtb",
+    api_secret=creds.api_secret
+)
+print(session_auth.get_wallet_balance(coin="USDT"))
