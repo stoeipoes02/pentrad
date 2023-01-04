0e72daa0928a6d67589b94644eefcf5eef6094eb
kick <kickbuur@gmail.com>
2023-01-04 16:41:08 +0100

    time tests with requests mark-price-kline

--- trading.py
+++ trading.py
@@ -1,14 +1,52 @@
-import time
-import sys
+import requests
+#import sys
 import bybit
+import time
+import datetime
 from pybit import inverse_perpetual
+import pandas as pd
+import calendar
 
-sys.path.append("../")
-import creds
+#raspberry pi
+#sys.path.append("../")
+#import creds
 
-session_auth = inverse_perpetual.HTTP(
-    endpoint="https://api.bybit.com",
-    api_key="vzgFHwAsjwMBhlRgtb",
-    api_secret=creds.api_secret
+#session_auth = inverse_perpetual.HTTP(
+#    endpoint="https://api.bybit.com",
+#    api_key="vzgFHwAsjwMBhlRgtb",
+#    api_secret=creds.api_secret
+#)
+
+#testnet_session_auth
+session = inverse_perpetual.HTTP(
+    endpoint='https://testnet.bybit.com',
+    api_key="Y30aFoxzTGQaCTybPC",
+    api_secret="WqCBpR6JfSSkzciYGslKC3fjcjlDrvjsLLWc"
 )
-print(session_auth.get_wallet_balance(coin="USDT"))
+
+def printbtcpricedaysago():
+    response1 = requests.get('https://api-testnet.bybit.com/derivatives/v3/public/mark-price-kline?category=linear&symbol=ETHUSDT&interval=D&start=1671145200000&end=1671318000000')
+    data = response1.json()
+    price = data['result']['list'][0]
+    return data
+
+print(printbtcpricedaysago())
+
+def print_btcprice():
+    response = requests.get("https://api.bybit.com/derivatives/v3/public/tickers?category=linear&symbol=BTCUSDT")
+    data = response.json()
+    price = data['result']['list'][0]['lastPrice']
+    return price
+
+#for i in range(len(info['result'])):
+#    print(f"{info['result'][i]['symbol']:<30} {info['result'][i]['bid_price']}")
+#    if info['result'][i]['symbol'] == 'FTMUSDT':
+#        print('yeeea found it', i)
+
+#print(session_auth.place_active_order(
+#    symbol="XRPUSD",
+#    side="Buy",
+#    order_type="Market",
+#    qty=1,
+#    time_in_force="GoodTillCancel"
+#))
