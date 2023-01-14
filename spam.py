65e72062b13d5216b4471a5c12df412f764d0578
kick <kickbuur@gmail.com>
2023-01-14 10:12:29 +0100

    visual studio test error

--- spam.py
+++ spam.py
@@ -3,8 +3,11 @@ import time
 
 words = ['river','transfer', 'skyscraper', 'commercial', 'food', 'elementary school','tree','pen', 'roof', 'church', 'clothes', 'bird', 'telephone']
 
+time.sleep(1)
+location = pyautogui.position()
+
 for i in range(len(words)):
-    pyautogui.click(3028,158)
+    pyautogui.click(location)
     pyautogui.typewrite(words[i])
     pyautogui.press("enter")
     for i in range(len(words[i])):
