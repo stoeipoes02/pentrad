83fd2b9dceff9eaff63b1334327623aa3c8fd843
kick <kickbuur@gmail.com>
2023-01-28 15:26:06 +0100

    today's project delete

--- app.py
+++ app.py
@@ -32,47 +32,8 @@ def index():
 
 @app.route('/trading')
 def trading():
-    pattern  = request.args.get('pattern', False)
-    stocks = {}
 
-    with open('datasets/symbols.csv') as f:
-        for row in csv.reader(f):
-            stocks[row[0]] = {'company': row[1]}
-
-    if pattern:
-        for filename in os.listdir('datasets/daily'):
-            df = pandas.read_csv('datasets/daily/{}'.format(filename))
-            pattern_function = getattr(talib, pattern)
-            symbol = filename.split('.')[0]
-
-            try:
-                results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
-                last = results.tail(1).values[0]
-
-                if last > 0:
-                    stocks[symbol][pattern] = 'bullish'
-                elif last < 0:
-                    stocks[symbol][pattern] = 'bearish'
-                else:
-                    stocks[symbol][pattern] = None
-            except Exception as e:
-                print('failed on filename: ', filename)
-
-    return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
-    
-@app.route('/snapshot')
-def snapshot():
-    with open('datasets/symbols.csv') as f:
-        for line in f:
-            if "," not in line:
-                continue
-            symbol = line.split(",")[0]
-            data = yf.download(symbol, start="2020-01-01", end="2020-08-01")
-            data.to_csv('datasets/daily/{}.csv'.format(symbol))
-
-    return {
-        "code": "success"
-    }
+    return render_template('trading.html')
 
 @app.route('/<int:post_id>')
 def post(post_id):
