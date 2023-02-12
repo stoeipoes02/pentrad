2152e7e43ae4f1dcb004ae88d583e29787d9f990
kick <kickbuur@gmail.com>
2023-02-12 23:44:56 +0100

    candle recognition tryout

--- oscillators.py
+++ oscillators.py
@@ -1,7 +1,10 @@
 import requests
 import time
 import talib
-from numpy import array
+import numpy as np
+
+# Issues:
+# getdata link v3 instead of v5
 
 # Returns current time in milliseconds and the time of x seconds ago since epoch
 def timedelta(times=3600):
@@ -10,18 +13,16 @@ def timedelta(times=3600):
     return millisecnow, starttime
 
 
-#  
-def getdata():
-    startend = timedelta(times=86400*5)
-    symbol = "BTCUSDT"
-    interval = "D"
+# Gets the data of bybit api
+# symbol: coin to be traded
+# interval: timeframe of candles 1,3,5,15,30,60,120,240,360,720,D,W,M
+# startend: start of time until current time in seconds
+def getdata(symbol="BTCUSDT", interval=15,startend=3600):
+    startend = timedelta(startend)
     link = f'https://api-testnet.bybit.com/derivatives/v3/public/kline?category=linear&symbol={symbol}&interval={interval}&start={startend[1]}&end={startend[0]}'
     r = requests.get(link).json()
     data = r['result']['list']
     return data
-
-print(getdata())
-
 
 # Simple Moving Average that takes data and turns it into a moving average
 # data: data of price from kline request
@@ -32,11 +33,22 @@ def SMA(data, length):
     for count, value in enumerate(data):
         closes.append(float(value[4]))
     
-    arr = array(closes)
+    arr = np.array(closes)
     simple_MA = talib.SMA(arr, length)
     return simple_MA[length-1:]
 
-value = SMA(getdata(), 2)
-print(value)
+#value = SMA(getdata(), 2)
+#print(value)
 
-# print(talib.STOCH(high, low, close))
+
+# list comprehension
+word = getdata(startend=3600)
+empty = [(x[1:5]) for x in word]
+floaters = np.array([[float(x) for x in inner]for inner in empty])
+
+one = np.array(floaters[0][0])
+two = np.array(floaters[0][1])
+three = np.array(floaters[0][2])
+four = np.array(floaters[0][3])
+
+#integer = talib.CDLHAMMER(one,two,three,four)
