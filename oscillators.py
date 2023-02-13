4341ce8c2fe021c713e2cf6b0b06016dfb4e7685
kick <kickbuur@gmail.com>
2023-02-14 00:15:58 +0100

    dataframe filled

--- oscillators.py
+++ oscillators.py
@@ -2,9 +2,11 @@ import requests
 import time
 import talib
 import numpy as np
+import pandas as pd
 
 # Issues:
 # getdata link v3 instead of v5
+# list comprehension usage not in script
 
 # Returns current time in milliseconds and the time of x seconds ago since epoch
 def timedelta(times=3600):
@@ -24,6 +26,7 @@ def getdata(symbol="BTCUSDT", interval=15,startend=3600):
     data = r['result']['list']
     return data
 
+
 # Simple Moving Average that takes data and turns it into a moving average
 # data: data of price from kline request
 # length: moving average length which will be returned
@@ -37,18 +40,21 @@ def SMA(data, length):
     simple_MA = talib.SMA(arr, length)
     return simple_MA[length-1:]
 
-#value = SMA(getdata(), 2)
-#print(value)
 
+# creates dataframe with columns: time, open, high, low, close, volume and turnover
+# uses list comprehension to fill the right value to every row/column
+def dataframefill(startend):
+    blub = getdata(startend=startend)
+    data = pd.DataFrame(columns=['Time','Open','High','Low','Close','Volume', 'Turnover'])
 
-# list comprehension
-word = getdata(startend=3600)
-empty = [(x[1:5]) for x in word]
-floaters = np.array([[float(x) for x in inner]for inner in empty])
+    i = 0
+    for col in data.columns:
+        data[f'{col}'] = [(x[i])for x in blub]
+        i += 1
+    return data
 
-one = np.array(floaters[0][0])
-two = np.array(floaters[0][1])
-three = np.array(floaters[0][2])
-four = np.array(floaters[0][3])
+cum = dataframefill(3600)
+print(cum)
 
-#integer = talib.CDLHAMMER(one,two,three,four)
+sma = talib.SMA(cum['Close'],2)
+print(sma[1:])
