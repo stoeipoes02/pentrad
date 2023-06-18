c2b33bd2b913b03ec243727b7d679657458ceed3
kick <kickbuur@gmail.com>
2023-06-18 14:35:31 +0200

    setupforlinux shell file

---
+++ setuplinux.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/stoeipoes02/pentrad.git
+cd pentrad
+chmod +x githubpull.sh
+git config pull.rebase true
+
+# add cronjob to automatically pull every minute
+(crontab -l ; echo "* * * * * /usr/bin/sh /home/pi/pentrad/githubpull.sh") | crontab -
+
+
+pip install django
