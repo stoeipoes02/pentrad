68a9c368704bde81149204bb5be91a187f6b21f2
kick <kickbuur@gmail.com>
2023-06-19 18:16:49 +0200

    pull changes

--- setuplinux.sh
+++ setuplinux.sh
@@ -4,10 +4,17 @@ chmod +x githubpull.sh
 git config pull.rebase true
 
 # add cronjob to automatically pull every minute
-(crontab -l ; echo "* * * * * /usr/bin/sh /home/pi/pentrad/githubpull.sh") | crontab -
+(crontab -l ; echo "* * * * * /usr/bin/sh /home/pi/pentrad/githubpull.sh 2>/home/pi/pentrad/error.log") | crontab -
 
 
 pip install django
 django-admin startproject pentrad .
 
+# databse setup
+python3 manage.py inspectdb
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+
+# start server at local
 python3 manage.py runserver 8000
